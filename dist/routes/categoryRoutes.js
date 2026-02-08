ba93dc4e2c96e3f6e9d0be36edbd7c9a29753ca2
Srijan Acharya <watckfreetv22@gmail.com>
2026-02-08 15:51:14 +0000

    Remove node_modules, dist, and .env from repository

--- dist/routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const categoryController_1 = require("../controllers/categoryController");
-const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.post('/check', authMiddleware_1.authMiddleware, categoryController_1.checkCategory);
-router.post('/', authMiddleware_1.authMiddleware, categoryController_1.createCategory);
-exports.default = router;
